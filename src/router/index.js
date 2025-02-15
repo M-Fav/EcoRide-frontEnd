237b0fd8572600f5ad5d292fd13ec6984c939bdb
MehdiFav <mehdi.favreau@gmail.com>
2025-02-15 11:52:43 +0100

    Importation accueil et généralisation de la méthode loginUser de backend-api

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginView from '../components/LoginView.vue'
-import TokenView from '../components/TokenView.vue' 
+import LoginView from '@/components/LoginView.vue'
+import TokenView from '@/components/TokenView.vue' 
+import AccueilView from '@/components/AccueilView.vue';
 
 const routes = [
   {
@@ -13,6 +14,11 @@ const routes = [
     name: 'token-display', 
     component: TokenView,
     props: route => ({ token: route.state?.token }) // récupératuon du token
+  },
+  {
+    path: '/accueil',
+    name: 'accueil',
+    component: AccueilView
   }
 ];
 
--- src/services/backend-api.js
+++ src/services/backend-api.js
@@ -4,11 +4,12 @@ import axios from "axios";
 const API_BASE_URL = "https://ecoride-service-4d84c02e7cd5.herokuapp.com";
 
 // Fonction pour l'authentification
-export async function loginUser(credentials) {
+export async function openBankingService(credentials, endpoint) {
   try {
-    const response = await axios.post(`${API_BASE_URL}/login`, credentials);
+    const response = await axios.post(`${API_BASE_URL}${endpoint}`, credentials);
     return response.data;
   } catch (error) {
     throw error.response?.data?.message || "Une erreur est survenue.";
   }
 }
+
